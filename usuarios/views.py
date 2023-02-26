d04397c52c84cb58f6ad30010d4749bd5cec5400
Alex Sobreira <axssxs00@gmail.com>
2023-02-26 03:36:20 -0300

    V - 26-02-2023 03:36

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,29 +1,27 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import CustomUsuarioCreateForm, LoginForm
 
 
 class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
-    redirect_field_name = 'ne'
 
 
 def login_view(request):
     if request.method == 'POST':
-        form_login = LoginForm(request.POST)
-        form_cadastro = CustomUsuarioCreateForm(request.POST)
-        if form_login.is_valid():
-            # processa o formulário de login
-            pass
-        if form_cadastro.is_valid():
-            # processa o formulário de cadastro
-            pass
-    else:
-        form_login = LoginForm()
-        form_cadastro = CustomUsuarioCreateForm()
-    return CustomLoginView.as_view()(request, **{'form': form_login})
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Email ou senha incorretos')
+    return render(request, 'registration/login.html', {'error': messages.get_messages(request)})
 
 
 @login_required(login_url='/login/')
